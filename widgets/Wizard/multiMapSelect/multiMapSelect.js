779202877d7f9a7c20ba710ed4b8fed2f340d487
aconyteds <ianwilson1337@gmail.com>
2015-01-16 20:11:37 +0000

    Fixed issues present with the graphics layer loading. MultiMap should now be working properly.

--- widgets/Wizard/multiMapSelect/multiMapSelect.js
+++ widgets/Wizard/multiMapSelect/multiMapSelect.js
@@ -1,7 +1,7 @@
 define(["dojo/_base/declare","dijit/_WidgetBase","dijit/_TemplatedMixin","dijit/_WidgetsInTemplateMixin", "dojo/text!./templates/multiMapSelect.html", "dojo/request/xhr",
-"esri/map",  "esri/graphic", "dijit/form/Button", "dojo/_base/array", "dojo/on", "dojo/dom-construct", "dojo/topic", "dojo/_base/lang",  "esri/graphicsUtils"],
+"esri/map",  "esri/graphic", "dijit/form/Button", "dojo/_base/array", "dojo/on", "dojo/dom-construct", "dojo/topic", "dojo/_base/lang",  "esri/geometry/Extent"],
 function(declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, templateString, xhr,
-    map,  Graphic, Button, array, on, domConstruct, topic, lang, graphicsUtils){
+    map,  Graphic, Button, array, on, domConstruct, topic, lang, Extent){
     var multiMapSelect=declare([ _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin],{
         widgetsInTemplate:true,
         templateString:templateString,
@@ -11,22 +11,19 @@ function(declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, templat
             xhr("widgets/Wizard/multiMapSelect/points.json", {handleAs: "json"}).then(function(data){
                 var length=me.selections;
                 while(length--){
-                    var gphc=new Graphic(data);
-                    var ext=graphicsUtils.graphicsExtent([gphc]);
-                    var myMap=new me._map({initExtent:ext}).placeAt(me.mapContainer);
-                    on.once(myMap.map, "load", lang.hitch(myMap, function(){
-                        this.mapPausible.pause();
-                        this.map.graphics.add(gphc);
-                        this.map.setExtent(ext).then(lang.hitch(this, function(){
-                            this.mapPausible.resume();
-                        }));
-                    }));
+                    var tempData=data;
+                    tempData.symbol.size=(length+1)*4;
+                    var myMap=new me._map({gphc:new Graphic(tempData), _initExt:new Extent(tempData.extent)}).placeAt(me.mapContainer);
                 }
             });
         },
         _map:declare([_WidgetBase],{
+            _initExt:null,
             constructor:function(){
                 this.domNode=domConstruct.create("div", {style:"display:inline-block; padding-right:5px; margin-bottom:5px;"});
+            },
+            postMixInProperties:function(){
+                this.gphc._extent=this._initExt;
             },
             postCreate:function(){
                 var mapSize=200;
@@ -36,26 +33,37 @@ function(declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, templat
                     basemap:"satellite",
                     logo:false,
                     slider:false,
-                    showAttribution:false
+                    showAttribution:false,
+                    extent:this._initExt
                 });
                 this.map.attr("style", "height:"+mapSize+"px; width:"+mapSize+"px; display:inline-block;");
-                this.map.resize();
+                
+                //Create Button to Select Map
+                this.selectButton=new Button({label:"Select", style:"display:block; text-align:center;"}).placeAt(this.domNode);
+                
+                //Setup On Load Handler
+                on.once(this.map, "load", lang.hitch(this, function(){
+                    this.map.graphics.add(this.gphc);
+                    this.map.resize();
+                    this.map.reposition();
+                    setTimeout(lang.hitch(this, function() {this._initHandlers();}), 2000);
+                }));
+            },
+            _initHandlers:function(){
                 //Attach Broadcaster
-                this.mapPausible=on.pausable(this.map, "extent-change", function(){
+                var mapPausable=on.pausable(this.map, "extent-change", function(){
                     topic.publish("multiMapSelect/extent-change", {id:this.id, extent:this.extent});
                 });
+                
                 //Attach Listener
                 topic.subscribe("multiMapSelect/extent-change", lang.hitch(this, function(data){
                     if(this.map.id!==data.id&&this.map.extent!==data.extent){
-                       this. mapPausible.pause();
+                        mapPausable.pause();
                         this.map.setExtent(data.extent).then(lang.hitch(this, function(){
-                            this.mapPausible.resume();
+                            mapPausable.resume();
                         }));
-                        
                     }
                 }));
-                //Create Button to Select Map
-                this.selectButton=new Button({label:"Select", style:"display:block; text-align:center;"}).placeAt(this.domNode);
             }
         })
     });
